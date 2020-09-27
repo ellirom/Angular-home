381fa2e05171bdc4466ce3494fabd0b2789a830d
ellirom <ellirom21@gmail.com>
2020-09-27 19:04:41 +0300

    home1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+title = 'HomeWork';
+numberLesson = 1;
+
+homeworks = [{
+  name:"Виконати домашнэ завдання 1 -", 
+  status: true
+},
+{
+  name:"Виконати домашнэ завдання 2 -", 
+  status: false
+},
+{
+  name:"Виконати домашнэ завдання 3 -", 
+  status: true
+},
+{
+  name:"Виконати домашнэ завдання 4 -", 
+  status: false
+},
+{
+  name:"Виконати домашнэ завдання 5 -", 
+  status: true
+}
+];
+
 
 }
