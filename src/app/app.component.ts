1ad5297cb07fb67bdb39bdd96e0d494969194168
ellirom <ellirom21@gmail.com>
2020-09-29 17:05:56 +0300

    homework2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -7,29 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 title = 'HomeWork';
-numberLesson = 1;
+numberLesson = 2 ;
 
-homeworks = [{
+homeworks: Task [];
+ngOnInit() {
+this.homeworks = [{
+  id: 0,
   name:"Виконати домашнэ завдання 1 -", 
   status: true
 },
-{
+{ 
+  id: 1,
   name:"Виконати домашнэ завдання 2 -", 
   status: false
 },
 {
+  id: 2,
   name:"Виконати домашнэ завдання 3 -", 
   status: true
 },
 {
+  id: 3,
   name:"Виконати домашнэ завдання 4 -", 
   status: false
 },
 {
+  id: 4,
   name:"Виконати домашнэ завдання 5 -", 
+  status: true
+},
+{
+  id: 5,
+  name:"Виконати домашнэ завдання 6 -", 
+  status: false
+},
+{
+  id: 6,
+  name:"Виконати домашнэ завдання 7 -", 
   status: true
 }
 ];
 
 
-}
+}}
